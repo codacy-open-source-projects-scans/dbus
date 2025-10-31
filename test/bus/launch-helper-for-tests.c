61bf134aa69ba8639a08c37dfc18507e427384bf
Codacy Archive <codacy-archive@local>
2025-10-31 01:52:30 +0000

    snapshot 2025-10-31T01:52:30.654846+00:00

---
+++ test/bus/launch-helper-for-tests.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2018 Collabora Ltd.
+ * SPDX-License-Identifier: MIT
+ *
+ * Embed a version of the real activation helper that has been altered
+ * to be testable. We monkey-patch it like this because we don't want to
+ * compile test-only code into the real setuid executable, and Automake
+ * versions older than 1.16 can't cope with expanding directory variables
+ * in SOURCES when using subdir-objects. */
+#define ACTIVATION_LAUNCHER_TEST
+#include "bus/activation-helper.c"
+#include "bus/activation-helper-bin.c"
