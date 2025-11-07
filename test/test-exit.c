f4e1d6c8323742cc3a18ac2806b648422af0f56e
Codacy Archive <codacy-archive@local>
2025-11-07 15:28:51 +0000

    snapshot 2025-11-07T15:28:51.579789+00:00

---
+++ test/test-exit.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2003 Red Hat, Inc.
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "config.h"
+
+/* This is a process that just exits with a failure code */
+int
+main (int argc, char **argv)
+{
+
+  return 1;
+}
